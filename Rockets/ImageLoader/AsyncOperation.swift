c47ddd9bfa3f9e9d5aa2d6e85b186e12ffc437f2
Suto, Evelyne <evelyne.suto@garmin.com>
2021-02-21 01:08:30 +0200

    Create asyncoperation to handle download operations

---
+++ Rockets/ImageLoader/AsyncOperation.swift
@@ -0,0 +1,55 @@
+//
+//  AsyncOperation.swift
+//  Rockets
+//
+//  Created by Suto, Evelyne on 20/02/2021.
+//
+
+import Foundation
+
+// Async operation subclasses need to override the execute function and call the completionBlock once the operation finished
+class AsyncOperation: Operation {
+    enum OperationState : Int {
+        case ready
+        case executing
+        case finished
+    }
+    
+    private var state : OperationState = .ready {
+        willSet {
+            self.willChangeValue(forKey: "isExecuting")
+            self.willChangeValue(forKey: "isFinished")
+        }
+        
+        didSet {
+            self.didChangeValue(forKey: "isExecuting")
+            self.didChangeValue(forKey: "isFinished")
+        }
+    }
+    
+    private(set) executeCompleted: (() -> Void)?
+    
+    override init() {
+        super.init()
+        self.executeCompleted = { [weak self] in
+            self?.state = .finished
+        }
+    }
+    
+    // override in subclass
+    func execute() { }
+    
+    override func start() {
+        guard !isCancelled else {
+            state = .finished
+            return
+        }
+        
+        state = .executing
+        execute()
+    }
+    
+    override var isReady: Bool { return state == .ready }
+    override var isExecuting: Bool { return state == .executing }
+    override var isFinished: Bool { return state == .finished }
+}
