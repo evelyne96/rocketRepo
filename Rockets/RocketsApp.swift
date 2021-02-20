d190f86712d197ecef5e83ae5b042509934f1e52
Suto, Evelyne <evelyne.suto@garmin.com>
2021-02-20 15:27:34 +0200

    Initial Commit

---
+++ Rockets/RocketsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RocketsApp.swift
+//  Rockets
+//
+//  Created by Suto, Evelyne on 20/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct RocketsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
